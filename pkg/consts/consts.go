09261530ddc4a6891886bb511a8677641402d62c
XinceChan <96606103+XinceChan@users.noreply.github.com>
2023-02-03 14:08:38 +0800

    更新基础文件

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -1,1 +1,10 @@
 package consts
+
+const (
+	MySQLDefaultDSN = "zhgg:zhgg@tcp(127.0.0.1:33069)/zhgg-dy?charset=utf8mb4&parseTime=true&loc=Local"
+
+	UserTableName  = "user"
+	VideoTableName = "video"
+
+	ETCDAddress = "127.0.0.1:2379"
+)
