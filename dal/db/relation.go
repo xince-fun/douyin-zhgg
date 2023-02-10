6093429074bde66a372f3e618f92c7a26ff28880
melrose12138 <wyz20000223@gmail.com>
2023-02-10 14:08:15 +0800

    点赞功能完善

--- dal/db/relation.go
+++ dal/db/relation.go
@@ -3,6 +3,7 @@ package db
 import (
 	"ByteTech-7815/douyin-zhgg/pkg/consts"
 	"context"
+
 	"github.com/cloudwego/kitex/pkg/klog"
 	"gorm.io/gorm"
 )
@@ -131,4 +132,19 @@ func GetFriendsUsers(ctx context.Context, userID int64) ([]*User, error) {
 		klog.Error("error occurred when get friends ids", err)
 	}
 	return QueryUserById(ctx, friendsIDs)
+}
+
+//根据当前用户id和目标用户id获取关注信息
+func QueryRelationByIds(ctx context.Context, currentId int64, userIds []int64) (map[int64]*Follow, error) {
+	var relations []*Follow
+	err := DB.WithContext(ctx).Where("user_id = ? AND to_user_id IN ?", currentId, userIds).Find(&relations).Error
+	if err != nil {
+		klog.Error("query relation by ids " + err.Error())
+		return nil, err
+	}
+	relationMap := make(map[int64]*Follow)
+	for _, relation := range relations {
+		relationMap[relation.FolloweeID] = relation
+	}
+	return relationMap, nil
 }
